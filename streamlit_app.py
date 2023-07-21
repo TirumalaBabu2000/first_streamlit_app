06c298fa3680c595b8408d7b80c2d975053b74b6
TirumalaBabu2000 <44167615+TirumalaBabu2000@users.noreply.github.com>
2023-07-21 13:43:22 +0530

    Update streamlit_app.py
    selection with fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 furits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+furits_list = furits_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(furits_list.index))
 
