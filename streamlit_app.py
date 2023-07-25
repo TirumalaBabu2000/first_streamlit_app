fd565be1448e74107c55d02273999a86009cf43e
TirumalaBabu2000 <44167615+TirumalaBabu2000@users.noreply.github.com>
2023-07-25 12:17:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas as pd
 import requests as re
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
